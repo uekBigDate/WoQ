36f7e0e2d2929f94da6b2f35b2b39afddc362f50
tankda <635291378@qq.com>
2019-01-09 19:23:54 +0800

    rm-f

--- Woq/woquApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WoquappConfig(AppConfig):
-    name = 'woquApp'
--- Woq/woquApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = "woqu"
-
-urlpatterns = [
-    path("login/",views.login,name="login"),   # 登录功能
-    path("register/",views.register,name="register"),   # 注册功能
-]
